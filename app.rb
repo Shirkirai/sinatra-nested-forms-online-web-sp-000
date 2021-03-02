f1efabc6b3e0a2d61fec7d7873015bb8ce79dde1
Jung Lee <lee.jung.s95@gmail.com>
2021-03-02 19:24:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,28 +17,11 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-
       params[:pirate][:ships].each do |info|
         Ship.new(info)
       end
-
       @ships = Ship.all
-=begin #=>
-pry(#<FormsLab::App>)> @ships =>
-[#<Ship:0x0000000002744d28
-  @booty="Silver",
-  @name="Titanic",
-  @type="Steam Liner">,
- #<Ship:0x0000000002744c38
-  @booty="Inner Tubes",
-  @name="Carpathia",
-  @type="Rescue Ship">]
-=end
-      #@ships will list an array with 2 ship instances. One of them is named Titanic
-      #the second ship is named Carpathia.
-      #I need to create 2 distinct objects and call out their name, type, and booty attributes
-  #    binding.pry
-
       erb :show
     end
+  end
 end
